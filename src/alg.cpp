054afade9036854ed36ed5fc405745d5f62a52a0
Burkov06 <s.burkov.2006@gmail.com>
2025-05-04 19:50:35 +0300

    ADS6.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,50 @@
 // Copyright 2021 NNTU-CS
+#pragma once
+
+template <typename T>
+class TPQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& d) : data(d), next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (head) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+
+    void push(const T& value) {
+        Node* newNode = new Node(value);
+        if (!head || value.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while (current->next && current->next->data.prior >= value.prior) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    T pop() {
+        if (!head)
+            throw std::runtime_error("Queue is empty");
+        Node* tmp = head;
+        T result = head->data;
+        head = head->next;
+        delete tmp;
+        return result;
+    }
+};
