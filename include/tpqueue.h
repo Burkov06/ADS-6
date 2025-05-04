6400fe993d6acc053855a5311a9e6778e4792ea2
Burkov06 <s.burkov.2006@gmail.com>
2025-05-04 19:58:09 +0300

    ADS6 tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,55 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+#ifndef TPQUEUE_H
+#define TPQUEUE_H
 
-template<typename T>
+#include <stdexcept>
+
+template <typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(const T& d) : data(d), next(nullptr) {}
+  };
+
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (head) {
+      Node* tmp = head;
+      head = head->next;
+      delete tmp;
+    }
+  }
+
+  void push(const T& value) {
+    Node* newNode = new Node(value);
+    if (!head || value.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      return;
+    }
+    Node* current = head;
+    while (current->next && current->next->data.prior >= value.prior) {
+      current = current->next;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+  }
+
+  T pop() {
+    if (!head)
+      throw std::runtime_error("Queue is empty");
+    Node* tmp = head;
+    T result = head->data;
+    head = head->next;
+    delete tmp;
+    return result;
+  }
 };
 
-struct SYM {
-  char ch;
-  int prior;
-};
-
-#endif  // INCLUDE_TPQUEUE_H_
+#endif  // TPQUEUE_H
